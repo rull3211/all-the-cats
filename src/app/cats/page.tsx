0170c768d7ad3283c93a02d9845d515c5e999baa
Bence Daniel Szøke <benze00@gmail.com>
2025-12-18 07:48:16 +0100

    chore renaming to uppercase files

--- src/app/cats/page.tsx
+++ src/app/cats/page.tsx
@@ -1,5 +1,11 @@
 import CatGrid from "@/src/features/cats/components/CatGridClient";
 import { fetchCats } from "@/src/server/externalApi/fetchCatsFromApi.api";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "All The Cats",
+  description: "A comprehensive collection of all the cats",
+};
 
 export default async function Cats() {
   const cats = await fetchCats({ limit: 20 });
